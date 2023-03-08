3a096ea6268c3f1908a34cf570cf5bbc9904691b
zabukovnikmaj <maj.zabukovnik@aspiria.com>
2023-03-08 07:51:40 +0100

    Updating products.php model

--- Models/Products.php
+++ Models/Products.php
@@ -4,5 +4,14 @@ namespace Models;
 
 class Products
 {
-
+    public string $name;
+    public string $description;
+    public double $price;
+    public string $date;
+    public function SavingData(array $data): void{
+        $name = $data['name'];
+        $description = $data['description'];
+        $price = doubleval($data['price']);
+        $date = $data['date'];
+    }
 }
