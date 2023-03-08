368a2394dd4ff93cbe5cdb091e99ea4c1f8368c6
zabukovnikmaj <maj.zabukovnik@aspiria.com>
2023-03-08 16:20:14 +0100

    adding base model for saving data

---
+++ Models/BaseModel.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Models;
+
+abstract class BaseModel
+{
+    public function savingData(): void{
+        $directory = __DIR__ . '/../data/' . basename(get_class($this)) . '.json';
+
+        $data = json_decode(file_get_contents($directory), true);
+
+        $new_entry = [];
+        foreach (get_object_vars($this) as $key => $value) {
+            if(is_array($value)){
+                $array = [];
+                foreach($value as $item){
+                    if(!empty($item)) {
+                        $array[] = $item;
+                    }
+                }
+                $value = $array;
+            }
+            $new_entry[$key] = $value;
+        }
+
+        $data[] = $new_entry;
+
+        file_put_contents($directory, json_encode($data,JSON_PRETTY_PRINT));
+
+    }
+}
