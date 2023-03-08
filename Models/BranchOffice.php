b4126d6722b944558f735a6dd322e5f6ec6f9505
zabukovnikmaj <maj.zabukovnik@aspiria.com>
2023-03-08 07:36:21 +0100

    Adding code for saving branch office data

--- Models/BranchOffice.php
+++ Models/BranchOffice.php
@@ -4,5 +4,13 @@ namespace Models;
 
 class BranchOffice
 {
+    public string $name;
+    public string $address;
+    public array $products;
 
+    public function SavingData($data): void{
+        $name = $data['name'];
+        $address = $data['address'];
+        $products = $data['products'];
+    }
 }
