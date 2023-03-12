1779205daecc4b561b8026da76b0a12d5b571b56
zabukovnikmaj <maj.zabukovnik@aspiria.com>
2023-03-12 10:33:39 +0100

    adding function for processing data

--- Controllers/Employees.php
+++ Controllers/Employees.php
@@ -1,10 +1,26 @@
 <?php
 
 namespace Controllers;
+use Models\Employees as EmployeesModel;
 
-class Employees
+class Employees extends BaseController
 {
     public function employees(): void{
         include_once __DIR__ . '/../views/employees/employeesForm.php';
+    }
+    public function processData(): bool{
+        if(!empty($this->findErrors()){
+            return false;
+        }
+
+        $employeesModel = new EmployeesModel();
+        $employeesModel->branchOffice = $_POST['branchOffice'];
+        $employeesModel->name = $_POST['name'];
+        $employeesModel->position = $_POST['position'];
+        $employeesModel->age = intval($_POST['age']);
+        $employeesModel->sex = $_POST['sex'];
+        $employeesModel->email = $_POST['email'];
+
+        return true;
     }
 }
