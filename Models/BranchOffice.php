67aa101989d9b0e1abb11f50808ebae274112add
zabukovnikmaj <maj.zabukovnik@aspiria.com>
2023-03-12 10:36:45 +0100

    getting rid of bad practice

--- Models/BranchOffice.php
+++ Models/BranchOffice.php
@@ -2,15 +2,11 @@
 
 namespace Models;
 
-class BranchOffice
+class BranchOffice extends BaseModel
 {
     public string $name;
     public string $address;
     public array $products;
 
-    public function SavingData($data): void{
-        $name = $data['name'];
-        $address = $data['address'];
-        $products = $data['products'];
-    }
+        //TODO: all letters should be lowercase for classes and methods, consistency, do not duplicate code -  base model
 }
