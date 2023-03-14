283e6876af662da54439995f7bccf86c9e894b59
zabukovnikmaj <maj.zabukovnik@aspiria.com>
2023-03-14 20:15:04 +0100

    adding ability to show errors

--- Controllers/Products.php
+++ Controllers/Products.php
@@ -6,14 +6,17 @@ use Models\Products as ProductsModel;
 
 class Products extends BaseController
 {
-    public function products(): void{
+    public function products(array $err = []): void{
         include_once __DIR__ . '/../views/products/productForm.php';
     }
 
     public function processData(): void{
-        if(!empty($this->findErrors())){
+        $err = $this->findErrors();
+        if(!empty($err)){
+            $this->products($err);
             return;
         }
+        header('Location: /');
         $productsModes = new ProductsModel();
         $productsModes->name = $_POST['name'];
         $productsModes->date = $_POST['deliveryDate'];
