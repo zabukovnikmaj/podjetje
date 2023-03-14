86ca2440be40f0905de1f8faae20ae232f369854
zabukovnikmaj <maj.zabukovnik@aspiria.com>
2023-03-14 16:30:38 +0100

    resolving issues

--- Controllers/Employees.php
+++ Controllers/Employees.php
@@ -1,10 +1,25 @@
 <?php
 
 namespace Controllers;
+use Models\Employees as EmployeesModel;
 
-class Employees
+class Employees extends BaseController
 {
     public function employees(): void{
         include_once __DIR__ . '/../views/employees/employeesForm.php';
+    }
+
+    public function processData(): void{
+        if(!empty($this->findErrors())){
+            return;
+        }
+        $employeesModel = new EmployeesModel();
+        $employeesModel->branchOffice = $_POST['branchOffice'];
+        $employeesModel->name = $_POST['name'];
+        $employeesModel->position = $_POST['position'];
+        $employeesModel->age = intval($_POST['age']);
+        $employeesModel->sex = $_POST['sex'];
+        $employeesModel->email = $_POST['email'];
+        $employeesModel->savingData();
     }
 }
