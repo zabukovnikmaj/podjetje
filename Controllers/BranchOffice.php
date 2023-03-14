f059fdff0b67c47963319cf7905d6b70744fa910
zabukovnikmaj <maj.zabukovnik@aspiria.com>
2023-03-14 16:22:38 +0100

    resolving confilct

--- Controllers/BranchOffice.php
+++ Controllers/BranchOffice.php
@@ -1,10 +1,24 @@
 <?php
-
 namespace Controllers;
+use Models\BranchOffice as BranchOfficeModels;
 
-class BranchOffice
+class BranchOffice extends BaseController
 {
     public function branchOffice(): void{
         include_once __DIR__ . '/../views/branchOffice/branchOfficeForm.php';
+    }
+    public function processData(): bool{
+        if(!empty($this->findErrors())){
+            return false;
+        }
+
+        $branchOfficeModel = new BranchOfficeModels();
+        $branchOfficeModel->name = $_POST['name'];
+        $branchOfficeModel->address = $_POST['address'];
+        $branchOfficeModel->products = $_POST['products'];
+
+        $branchOfficeModel->savingData();
+
+        return true;
     }
 }
