a68d83aba79b63a06d6dd239155a6b9e6a3581de
zabukovnikmaj <maj.zabukovnik@aspiria.com>
2023-03-14 16:36:55 +0100

    adding missing lines for saving data

--- Controllers/Products.php
+++ Controllers/Products.php
@@ -2,9 +2,23 @@
 
 namespace Controllers;
 
-class Products
+use Models\Products as ProductsModel;
+
+class Products extends BaseController
 {
     public function products(): void{
         include_once __DIR__ . '/../views/products/productForm.php';
+    }
+
+    public function processData(): void{
+        if(!empty($this->findErrors())){
+            return;
+        }
+        $productsModes = new ProductsModel();
+        $productsModes->name = $_POST['name'];
+        $productsModes->date = $_POST['date'];
+        $productsModes->price = doubleval($_POST['price']);
+        $productsModes->description = $_POST['description'];
+        $productsModes->savingData();
     }
 }
