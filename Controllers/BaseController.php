d3ce6ee9d65db9abce4f4becd75feb8c72bfa628
zabukovnikmaj <maj.zabukovnik@aspiria.com>
2023-03-21 17:09:01 +0100

    getting rid of duplicate method

--- Controllers/BaseController.php
+++ Controllers/BaseController.php
@@ -4,17 +4,5 @@ namespace Controllers;
 
 abstract class BaseController
 {
-    /**
-     * @return array
-     * general method for checking if any of $_POST elements are empty and returning array of errors
-     */
-    public function findErrors(): array{
-        $err = [];
-        foreach($_POST as $key => $value){
-            if(empty($value)){
-                $err[] = "The value of " . $key . ' cannot be empty!';
-            }
-        }
-        return $err;
-    }
+    
 }
