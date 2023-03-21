45e1c6d10f3c199c4265eabacf87685a39b9fee0
zabukovnikmaj <maj.zabukovnik@aspiria.com>
2023-03-21 15:40:30 +0100

    adding constant variables

--- public/index.php
+++ public/index.php
@@ -2,6 +2,10 @@
 
 use Services\Router;
 
+const BASE_PATH = __DIR__ . '/../';
+
+require_once BASE_PATH . 'Services/Functions.php';
+
 // Autoloader function
 spl_autoload_register(function ($class) {
     require_once __DIR__ . '/../' . str_replace("\\", "/", $class) . '.php';
