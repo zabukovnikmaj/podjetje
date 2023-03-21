30a5feec4d156a93eda8ed4427bb40292247840e
zabukovnikmaj <maj.zabukovnik@aspiria.com>
2023-03-21 16:43:58 +0100

    adding links to other pages

--- views/index/index.php
+++ views/index/index.php
@@ -6,5 +6,8 @@
 </head>
 <body>
     <h1>Company</h1>
+    <a href="/branchOffice/list">Seznam fakultet</a>
+    <a href="/employees/list">Seznam zaposlenih</a>
+    <a href="/products/list">Seznam predmetov</a>
 </body>
 </html>
