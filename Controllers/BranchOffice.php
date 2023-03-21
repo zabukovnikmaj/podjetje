991ac466d28d9e1c5c101be97f29cb9c594e2dab
zabukovnikmaj <maj.zabukovnik@aspiria.com>
2023-03-21 15:37:57 +0100

    bug fixes - code reformating

--- Controllers/BranchOffice.php
+++ Controllers/BranchOffice.php
@@ -1,15 +1,20 @@
 <?php
+
 namespace Controllers;
+
 use Models\BranchOffice as BranchOfficeModels;
 
 class BranchOffice extends BaseController
 {
-    public function branchOffice(array $err = []): void{
+    public function branchOffice(array $err = []): void
+    {
         include_once __DIR__ . '/../views/branchOffice/branchOfficeForm.php';
     }
-    public function processData(): void{
+
+    public function processData(): void
+    {
         $err = $this->findErrors();
-        if(!empty($err)){
+        if (!empty($err)) {
             $this->branchOffice($err);
             return;
         }
