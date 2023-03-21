fdd4be863cedcdd4933b4d4a46edf22561a4fc75
zabukovnikmaj <maj.zabukovnik@aspiria.com>
2023-03-21 16:55:36 +0100

    bug fixes

--- views/index/index.php
+++ views/index/index.php
@@ -6,8 +6,8 @@
 </head>
 <body>
     <h1>Company</h1>
-    <a href="/branchOffice/list">List branch offices</a> <br>
-    <a href="/employees/list">List employees</a> <br>
-    <a href="/products/list">List products</a> <br>
+    <a href="/branchOffice/list/">List branch offices</a> <br>
+    <a href="/employees/list/">List employees</a> <br>
+    <a href="/products/list/">List products</a> <br>
 </body>
 </html>
