bc98429d9da3cad9543d43c6b8405db020a2e362
zabukovnikmaj <maj.zabukovnik@aspiria.com>
2023-03-21 15:44:27 +0100

    adding view for displaying data

---
+++ views/branchOffice/list.php
@@ -0,0 +1,25 @@
+<p>
+    <a href="/faculty/create">New branch office</a>
+</p>
+
+<table>
+    <thead>
+    <tr>
+        <th>Name</th>
+        <th>Address</th>
+        <th>Products</th>
+        <th></th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>primer imena</td>
+        <td>primer naslova</td>
+        <td>primer produktov</td>
+        <td>
+            <a href="">Uredi</a>
+            <a href="">Izbriši</a>
+        </td>
+    </tr>
+    </tbody>
+</table>
