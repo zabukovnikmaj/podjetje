0b287ba7e1d738142f740d0b346620a74d1ac3af
zabukovnikmaj <maj.zabukovnik@aspiria.com>
2023-03-21 17:30:59 +0100

    code reformating

--- Controllers/BaseController.php
+++ Controllers/BaseController.php
@@ -4,5 +4,5 @@ namespace Controllers;
 
 abstract class BaseController
 {
-
+    //this controller is now redundant, but I will keep it in case I need it in the future
 }
--- Models/BaseModel.php
+++ Models/BaseModel.php
@@ -8,15 +8,16 @@ abstract class BaseModel
      * @return void
      * generalized method for saving all data from class variables to .json file with the same name as class
      */
-    public function savingData(): void{
+    public function savingData(): void
+    {
         $directory = __DIR__ . '/../data/' . basename(get_class($this)) . '.json';
         $data = json_decode(file_get_contents($directory), true);
         $new_entry = [];
         foreach (get_object_vars($this) as $key => $value) {
-            if(is_array($value)){
+            if (is_array($value)) {
                 $array = [];
-                foreach($value as $item){
-                    if(!empty($item)) {
+                foreach ($value as $item) {
+                    if (!empty($item)) {
                         $array[] = $item;
                     }
                 }
@@ -25,6 +26,6 @@ abstract class BaseModel
             $new_entry[$key] = $value;
         }
         $data[] = $new_entry;
-        file_put_contents($directory, json_encode($data,JSON_PRETTY_PRINT));
+        file_put_contents($directory, json_encode($data, JSON_PRETTY_PRINT));
     }
 }
