367f9103cb644bbf1950990375e2fbe1aee03cee
zabukovnikmaj <maj.zabukovnik@aspiria.com>
2023-04-04 16:37:30 +0200

    adding uuid method

--- Services/Functions.php
+++ Services/Functions.php
@@ -34,3 +34,23 @@ function view(string $path, array $attributes = []): void
 
     include base_path("views/" . $path . ".php");
 }
+
+/**
+ * Method generates unique UUID
+ *
+ * @return string
+ * @throws Exception
+ */
+function uuid(): string
+{
+    // Generate 16 bytes (128 bits) of random data or use the data passed into the function.
+    $data = random_bytes(16);
+
+    // Set version to 0100
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+    // Set bits 6-7 to 10
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+    // Output the 36 character UUID.
+    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+}
