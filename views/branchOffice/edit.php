d0a1d52bfc2301753b73d418a896fee3f3c380bd
zabukovnikmaj <maj.zabukovnik@aspiria.com>
2023-04-15 10:17:22 +0200

    code for keeping form data even with errors on edits

--- views/branchOffice/edit.php
+++ views/branchOffice/edit.php
@@ -15,8 +15,7 @@ view('partials/errors', [
     <input type="text" name="address" value="<?php echo htmlspecialchars($filteredData['address']); ?>"><br><br>
 
     <label for="products">Products name</label><br>
-    <textarea name="products" cols="50" rows="4"><?php echo htmlspecialchars(implode(', ', $filteredData['products'])); ?></textarea> <br>
-    <br>
+    <textarea name="products" cols="50" rows="4"><?php echo htmlspecialchars(implode(', ', $filteredData['products'])); ?></textarea> <br><br>
 
     <input type="submit" value="Save branch office data"> <br> <br>
 </form>
