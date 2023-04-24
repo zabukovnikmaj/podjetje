504a5c6c305450507f91c121af5746d95d14e036
zabukovnikmaj <maj.zabukovnik@aspiria.com>
2023-04-24 19:15:53 +0200

    adding put method

--- config/routes.php
+++ config/routes.php
@@ -54,10 +54,10 @@ return [
     ],
     '/employees/edit' => [
         'GET' => [Employees::class, 'displayEditItem'],
-        'POST' => [Employees::class, 'saveEditedData']
+        'PUT' => [Employees::class, 'saveEditedData']
     ],
     '/products/edit' => [
         'GET' => [Products::class, 'displayEditItem'],
-        'POST' => [Products::class, 'saveEditedData']
+        'PUT' => [Products::class, 'saveEditedData']
     ]
 ];
