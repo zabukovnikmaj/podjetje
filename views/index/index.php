9a2654bd896cd07b0ae130cc063ede600f536b0f
zabukovnikmaj <maj.zabukovnik@aspiria.com>
2023-04-29 11:39:21 +0200

    adding style to home page

--- views/index/index.php
+++ views/index/index.php
@@ -3,11 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <title>Company</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <style>
+        body {
+            padding: 20px;
+        }
+        h1 {
+            margin-bottom: 20px;
+        }
+        a {
+            display: block;
+            margin-bottom: 10px;
+        }
+    </style>
 </head>
 <body>
+<div class="container">
     <h1>Company</h1>
-    <a href="/branchOffice/list/">List branch offices</a> <br>
-    <a href="/employees/list/">List employees</a> <br>
-    <a href="/products/list/">List products</a> <br>
+    <a class="btn btn-primary" href="/branchOffice/list/">List branch offices</a>
+    <a class="btn btn-primary" href="/employees/list/">List employees</a>
+    <a class="btn btn-primary" href="/products/list/">List products</a>
+</div>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </body>
 </html>
