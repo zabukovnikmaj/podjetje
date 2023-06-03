b0a952f6a87fd083875ea5520c7d4770b76e939a
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-06-03 11:47:00 +0300

    adding function for file upload

--- Models/BaseModel.php
+++ Models/BaseModel.php
@@ -24,6 +24,10 @@ abstract class BaseModel
 
         $data[] = $new_entry;
         file_put_contents($directory, json_encode($data, JSON_PRETTY_PRINT));
+
+        if(!empty($_FILES['productFile']['tmp_name'])){
+            $this->saveImage(basename($className), $new_entry['uuid']);
+        }
     }
 
     /**
@@ -36,4 +40,28 @@ abstract class BaseModel
     {
         return uuid();
     }
+
+    /**
+     * function for saving image to data/files/$folderName/$fileName directory
+     *
+     * @param string $folderName
+     * @param string $filename
+     * @return bool
+     */
+    protected function saveImage(string $folderName, string $filename): bool
+    {
+        $fileDir = base_path('data/files/' . $folderName . '/');
+
+        if (!is_dir($fileDir)) {
+            mkdir($fileDir, 0755, true);
+        }
+
+        $targetPath = $fileDir . $filename . '.' . pathinfo($_FILES['productFile']['name'], PATHINFO_EXTENSION);
+
+        if (move_uploaded_file($_FILES['productFile']['tmp_name'], $targetPath)) {
+            return true;
+        }
+        return false;
+    }
+
 }
