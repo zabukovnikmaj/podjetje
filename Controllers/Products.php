91f0c39263cc8d317308e236de9b7e459a2e7916
Maj Zabukovnik <maj.zabukovnik1@gmail.com>
2023-06-03 12:07:12 +0300

    saving image file type to model

--- Controllers/Products.php
+++ Controllers/Products.php
@@ -59,6 +59,7 @@ class Products extends BaseController
         $productsModes->setDate(htmlspecialchars($_POST['deliveryDate']));
         $productsModes->setPrice(floatval(htmlspecialchars($_POST['price'])));
         $productsModes->setDescription(htmlspecialchars($_POST['description']));
+        $productsModes->setFileType(pathinfo($_FILES['productFile']['name'], PATHINFO_EXTENSION));
         $productsModes->setUuid();
         $productsModes->savingData();
         header('Location: /');
