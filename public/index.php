fd78f320048478bd5c0ec83376724787e6b92df3
zabukovnikmaj <maj.zabukovnik@aspiria.com>
2023-06-22 07:58:41 +0200

    defining constants

--- public/index.php
+++ public/index.php
@@ -6,6 +6,10 @@ const BASE_PATH = __DIR__ . '/../';
 
 $config = require BASE_PATH . 'config/config.php';
 define("CONFIG", $config);
+define("DATABASE_USERNAME", $config['username']);
+define("DATABASE_PASSWORD", $config['password']);
+define("DATABASE_NAME", $config['databaseName']);
+define("SERVER_NAME", $config['serverName']);
 
 require_once BASE_PATH . 'Services/Functions.php';
 
